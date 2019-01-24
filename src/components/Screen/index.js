ecb8e6e3997f043cbd3c5da98519d73e97c2dd48
@alexrili <alex@onload.com.br>
2019-01-24 06:09:56 -0200

    bugfix on android statusbar

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -7,7 +7,11 @@ export default ({ children }) => {
   return (
     <View style={styles.screen}>
       <SafeAreaView style={styles.safeArea} forceInset={{ top: 'always' }} />
-      <StatusBar translucent={true} backgroundColor="transparent" />
+      <StatusBar
+        translucent={true}
+        backgroundColor="transparent"
+        barStyle={'dark-content'}
+      />
       <View style={styles.content}>{children}</View>
       <SafeAreaView style={styles.safeArea} forceInset={{ top: 'bottom' }} />
     </View>
