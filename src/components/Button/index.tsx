482a77abb855455f8204eb8bff5f5fbfa76ba391
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-04-11 12:02:08 -0300

    feat: Button Component using styled component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import styled from 'theme'
+
+interface IProps {
+  border?: boolean
+  primary?: boolean
+  margin?: boolean
+}
+
+export const Button = styled.TouchableHighlight<IProps>`
+  background-color: ${props => props.border ? '#FFF' : props.theme.primary};
+  border: 2px solid ${props => props.theme.primary};
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+  margin: ${props=>props.margin? 10 : 0}px
+`
+
+export const ButtonText = styled.Text`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  text-align: center;
+  text-transform: uppercase;
+  color: ${props => !props.primary ? '#FFF' : props.theme.primary};
+`
