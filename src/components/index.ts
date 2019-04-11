07e5bc31de06f4b4a312357e6bd6aefd707f1b1c
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-04-11 12:04:01 -0300

    feat: import components inline

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+export * from './Button'
+import Text from './Text'
+import ProductListItem from './ProductListItem'
+import ProductImage from './ProductImage'
+import Container from './Container'
+
+export {
+  Text,
+  ProductListItem,
+  ProductImage,
+  Container
+}
