6d741eb504875e7686b13857c2826d931e189556
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-04-13 01:39:19 -0300

    fix: currency formatter BRL

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,9 @@
-export const formatReal: string | any = (int: number) =>
-  int.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+export const formatReal: string | any = (mixed: number, pre = 'R$ ') => {
+  let int = parseInt(mixed.toFixed(2).toString().replace(/[^\d]+/g, ''))
+  let tmp = int + ''
+  tmp = tmp.replace(/([0-9]{2})$/g, ',$1')
+  if (tmp.length > 6)
+    tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2')
+
+  return `${pre}${tmp}`
+}
