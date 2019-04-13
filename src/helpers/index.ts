4a1d428b4503159e479680748201be032f54b23b
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-04-13 13:00:09 -0300

    fix: lint helper/formatReal

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,5 @@
 export const formatReal: string | any = (mixed: number, pre = 'R$ ') => {
-  let int = parseInt(mixed.toFixed(2).toString().replace(/[^\d]+/g, ''))
-  let tmp = int + ''
+  let tmp = parseInt(mixed.toFixed(2).toString().replace(/[^\d]+/g, '')) + ''
   tmp = tmp.replace(/([0-9]{2})$/g, ',$1')
   if (tmp.length > 6)
     tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2')
