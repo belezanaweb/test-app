958edf164a7b4de7364107548a74cc07f51f4fcc
Fernando Belmonte <fernando.belmonte@live.com>
2020-01-11 21:07:43 -0300

    add: strings file

---
+++ src/constants/strings.js
@@ -0,0 +1,21 @@
+// GLOBAL
+export const CURRENCY = 'R$'
+
+// NAVIGATION HEADER TITLE
+export const LIST_VIEW = 'LISTA DE PRODUTOS'
+export const DETAIL_VIEW = 'DETALHES DO PRODUTO'
+
+// SCREENS
+// ListView
+export const LOAD_MORE = 'CARREGAR MAIS PRODUTOS'
+export const LOADING = 'CARREGANDO...'
+// DetailView
+export const OFF = 'de'
+export const DESC = 'Descrição do Produto'
+export const VIEW_MORE = 'Continuar lendo'
+export const BUY = 'COMPRE'
+export const WARN_ME = 'AVISE-ME'
+
+// COMPONENTS
+// ListItem
+export const DETAILS = 'VER DETALHES'
