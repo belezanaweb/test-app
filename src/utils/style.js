de3e7d9af08184564b680d358ec0619ed5e989b5
Leonardo <leonardo@jmartinsanjos.local>
2020-01-19 18:43:40 -0300

    finish request api
    Former-commit-id: 55fe1ebe11b3950fa0b5b4d6e7238aca348feac2

--- src/utils/style.js
+++ src/utils/style.js
@@ -12,3 +12,11 @@ export const ContainerScroll = styled.ScrollView`
 export const ContainerWrapper = styled.View`
 	padding: 0px 20px;
 `;
+
+export const ContainerLoading = styled.View`
+	flex: 1;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	background: rgba(255, 255, 255, 5);
+`;
