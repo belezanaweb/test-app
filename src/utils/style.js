a14f4bb1b68ad365e2eb547a65178711467eff9b
Leonardo <leonardo@jmartinsanjos.local>
2020-01-19 11:08:59 -0300

    defined styled main
    Former-commit-id: 7691c67a86331e33c313f347fc183057270f8200

--- src/utils/style.js
+++ src/utils/style.js
@@ -1,6 +1,14 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Container = styled.SafeAreaView`
 	flex: 1;
 	background: #f8f8f8;
-`
+`;
+
+export const ContainerScroll = styled.ScrollView`
+	flex: 1;
+`;
+
+export const ContainerWrapper = styled.View`
+	padding: 0px 20px;
+`;
