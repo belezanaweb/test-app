93fabeb30b61cdea18f63f8ba3ec9fc8700eadb4
Leonardo <leonardo@jmartinsanjos.local>
2020-01-19 11:12:10 -0300

    create screen prodDescription
    Former-commit-id: e3d905c7d274292187ed72fb5581b3618160cc1b

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -1,16 +1,14 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const ContainerHeader = styled.View`
 	height: 40px;
 	justify-content: center;
-`
+`;
 
 export const TextHeader = styled.Text`
 	font-family: 'Roboto-Medium';
-	font-style: normal;
-	font-weight: bold;
 	font-size: 14px;
 	line-height: 16px;
 	text-align: center;
 	text-transform: uppercase;
-`
+`;
--- src/screen/prodDesc/index.js
+++ src/screen/prodDesc/index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { Teste } from './style'
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Teste } from './style';
 
 export default class ProdDescription extends Component {
-  render() {
-    return (
-      <View>
-        <Teste> Prod Description </Teste>
-      </View>
-    )
-  }
+	render() {
+		return (
+			<View>
+				<Teste> Prod Description </Teste>
+			</View>
+		);
+	}
 }
