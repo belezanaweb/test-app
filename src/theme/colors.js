3d473d7b697b26889e12ad1ad887fcb1b92e35c4
Enieber Cunha <eniebercunha@gmail.com>
2020-04-10 11:57:59 -0300

    feat: create patte colors

---
+++ src/theme/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#FF6C00',
+  secundary: '#A43287',
+  dark: '#212121',
+  darkOpacity: '#6F6F6F',
+  light: '#FFFFFF',
+  transparent: 'transparent'
+}
+
+export default colors
