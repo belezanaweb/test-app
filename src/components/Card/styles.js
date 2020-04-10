0481e2dc7cbce9e0a6cc10e9822cfae58b414825
Enieber Cunha <eniebercunha@gmail.com>
2020-04-10 20:18:06 -0300

    fix: margin of Card component

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -6,8 +6,8 @@ export const Container = styled.View`
   background-color: ${colors.light};
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
   border-radius: 4px;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  padding-left: 10px;
-  padding-right: 10px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  margin-left: 10px;
+  margin-right: 10px;
 `
