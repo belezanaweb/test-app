76e8f2087ca2c9c6a27e75ed4332cca0831c963c
Rodrigo Fontes <fontesr2@gmail.com>
2020-04-20 08:45:07 -0300

    add: cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const logger = require('morgan')
 
@@ -6,6 +7,7 @@ const products = require('./routes/products')
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
