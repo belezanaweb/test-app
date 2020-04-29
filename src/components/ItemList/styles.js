9e4d078ad2a8bfffad0bc78767dddc885026aa2c
Enieber Cunha <eniebercunha@gmail.com>
2020-04-29 08:44:27 -0300

    feat: add padding horizontal in item list component

--- src/components/ItemList/styles.js
+++ src/components/ItemList/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components/native'
 export const Content = styled.View`
   flex-direction: row;
   justify-content: space-around;
+  padding-top: 10px;
+  padding-bottom: 10px;
 `
 
 export const ContentImage = styled.View`
