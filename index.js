445c9f05a50b8afdfaa7bc10792940da8cd9076b
Maycon Mesquita <mayconjcm@gmail.com>
2020-07-18 18:27:40 -0300

    fix: index.js imports

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native'
-import App from 'src/App'
-import { name as appName } from 'test_app/app.json'
+import App from './src/App'
+import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
