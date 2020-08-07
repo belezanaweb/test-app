3df65f3763ac7410dfb72be1fa3092f680f7d44d
Rodrigo Prado Fontes <fontesr2@gmail.com>
2020-08-07 14:08:06 -0300

    refactor: upgrade project to use RN 0.63.2 (#17)
    * refactor: upgrade project to use RN 0.63.2
    
    * remove: unwanted changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,35 @@
 module.exports = {
+  extends: '@react-native-community',
   root: true,
-  extends: '@react-native-community'
+  rules: {
+    'arrow-parens': ['error', 'as-needed'],
+    'brace-style': 'error',
+    'comma-dangle': ['error', 'never'],
+    'default-case': 'warn',
+    'key-spacing': ['error', { afterColon: true, beforeColon: false, mode: 'strict' }],
+    'new-cap': 'warn',
+    'no-cond-assign': 'error',
+    'no-console': 'warn',
+    'no-constant-condition': 'error',
+    'no-else-return': 'error',
+    'no-empty': 'error',
+    'no-extra-boolean-cast': 'error',
+    'no-extra-parens': 'off',
+    'no-inner-declarations': 'warn',
+    'no-lonely-if': 'warn',
+    'no-multi-spaces': 'error',
+    'no-nested-ternary': 'error',
+    'no-redeclare': 'error',
+    'no-undef': 'warn',
+    'no-undefined': 'off',
+    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTaggedTemplates: true }],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_', ignoreRestSiblings: true }],
+    'react/jsx-boolean-value': 'warn',
+    'react/jsx-sort-props': 'warn',
+    'react/jsx-wrap-multilines': 'warn',
+    'react/no-did-mount-set-state': 'off',
+    'react/no-did-update-set-state': 'off',
+    'react/prop-types': 'warn',
+    semi: ['error', 'never']
+  }
 }
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  jsxBracketSameLine: false,
+  parser: 'babel',
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
