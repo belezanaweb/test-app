1cac4dd372bb3db9d14536bb8b5681397c923152
Daniel <gleidson10daniel@hotmail.com>
2020-10-29 17:48:42 -0300

    add: onPress prop

--- src/components/Buttons/SecondaryButton.tsx
+++ src/components/Buttons/SecondaryButton.tsx
@@ -5,11 +5,12 @@ import { BaseButton, BaseButtonText } from './styles'
 
 interface ISecondaryButton {
   text?: string;
+  onPress?: () => void;
 }
 
-const SecondaryButton = ({ text }: ISecondaryButton) => {
+const SecondaryButton = ({ text, onPress }: ISecondaryButton) => {
   return (
-    <BaseButton background={theme.colors.white} borderColor={theme.colors.orange}>
+    <BaseButton background={theme.colors.white} borderColor={theme.colors.orange} onPress={onPress}>
       <BaseButtonText color={theme.colors.orange}>{text}</BaseButtonText>
     </BaseButton>
   )
