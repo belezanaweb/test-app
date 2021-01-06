27cac5909d941d6d3e58d1195971293610644415
Ramon Cruz <ramon.silva@ilia.digital>
2021-01-06 00:00:50 -0300

    feat: 🎸 Serviço
    Serviço auxiliar concluido

--- src/services/boticatio.api.ts
+++ src/services/boticatio.api.ts
@@ -1,4 +1,23 @@
 import service from './boticario.service';
 import { product } from '../util/types';
 
-const products = () => {};
+export const products = (page: number, size: number) =>
+  new Promise<Array<product>>((resolve, reject) => {
+    service
+      .get<Array<product>>('/products', {
+        params: {
+          page,
+          size,
+        },
+      })
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(error));
+  });
+
+export const productsDetail = (sku: string) =>
+  new Promise<product>((resolve, reject) => {
+    service
+      .get<product>(`/products/${sku}`)
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(error));
+  });
