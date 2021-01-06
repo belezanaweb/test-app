d692f32a53b8fb72439078ed6208bc75a5291d85
Allan Rodrigues <allan@startse.com>
2021-01-06 02:52:29 -0300

    refactor: change the formatCurrency function to work on Android without having to change the JavascriptCore flavor

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,4 +1,8 @@
-const formatCurrency = value =>
-  value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+const formatCurrency = value => {
+  value = parseFloat(value)
+  value = value.toFixed(2).split('.')
+  value[0] = 'R$ ' + value[0].split(/(?=(?:...)*$)/).join('.')
+  return value.join(',')
+}
 
 export default formatCurrency
