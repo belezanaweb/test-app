4ed196821f274df627046b0c6df11760bb133010
octavio tosta <--global>
2021-01-23 15:57:35 -0300

    feat: app labels

---
+++ src/utils/appLabels/index.ts
@@ -0,0 +1,12 @@
+/**
+ * I locked on the pr-br because the boticario test does not need more than that.
+ * If there was another file, in an internationalisation case, for example,
+ * this JSON could easily be consumed from some endpoint.
+ * I will just need update a little logic here and enjoy.
+ *
+ * -> can use i18n instead of this too :D
+ */
+
+import appLabels from './pt-BR.json';
+
+export default appLabels;
