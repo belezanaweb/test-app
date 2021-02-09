144993991489abe4e67a70506f6f1a0aeeca1442
octavio tosta <--global>
2021-02-08 21:03:33 -0300

    feat: logic to show accordion only if scrollview are higher then product container

--- src/containers/ProductDetail/components/ProductView/index.tsx
+++ src/containers/ProductDetail/components/ProductView/index.tsx
@@ -1,6 +1,8 @@
 import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
-import { LayoutRectangle, ScrollView } from 'react-native';
-import Product from 'shared/types/Product';
+import { LayoutRectangle, ScrollView, View } from 'react-native';
+import { moderateScale } from 'react-native-size-matters';
+import theme from 'shared/styles/theme';
+import Product from 'utils/types/Product';
 import AccordionButton from '../AccordionButton';
 import BuyButton from '../BuyButton';
 import ProductDescription from '../ProductDescription';
@@ -14,6 +16,9 @@ type ProductViewProps = {
 const ProductView: React.FC<ProductViewProps> = ({ product }) => {
   const [isAccordionOpen, setIsAccordionOpen] = useState(false);
   const [descriptionYAxisOffset, setDescriptionYAxisOffset] = useState(0);
+  const [scrollViewHeight, setScrollViewHeight] = useState(0);
+  const [contentScrollViewHeight, setcontentScrollViewHeight] = useState(0);
+  const [hasAccordion, setHasAccordion] = useState(false);
   const productViewRef = useRef<ScrollView>(null);
 
   const handleAccordionPress = useCallback(() => {
@@ -27,6 +32,13 @@ const ProductView: React.FC<ProductViewProps> = ({ product }) => {
     });
   }, [descriptionYAxisOffset, isAccordionOpen]);
 
+  useEffect(() => {
+    scrollViewHeight &&
+      contentScrollViewHeight &&
+      contentScrollViewHeight > scrollViewHeight &&
+      setHasAccordion(true);
+  }, [contentScrollViewHeight, scrollViewHeight]);
+
   const hasInventory = !!product.inventory.quantity;
 
   const handleDescriptionLayout = useCallback(
@@ -36,6 +48,14 @@ const ProductView: React.FC<ProductViewProps> = ({ product }) => {
     [],
   );
 
+  // I need use this offset because some descriptions don't have an corret HTML
+  // to react-native-render-html component render. I think HTML sanitize
+  // is not necessary for test, so I decided to add an offset avoid the cut at
+  // the bottom of the description. In normal cases, it is totally unnecessary.
+  const contentContainerStyle = {
+    paddingBottom: moderateScale(theme.dimensions.scrollView.paddingOffSet),
+  };
+
   return (
     <>
       <StyledScrollView
@@ -44,21 +64,28 @@ const ProductView: React.FC<ProductViewProps> = ({ product }) => {
         scrollEnabled={isAccordionOpen}
         showsVerticalScrollIndicator={isAccordionOpen}
         removeClippedSubviews={true}
-        contentContainerStyle={{ paddingBottom: 20 }}
         onLayout={({ nativeEvent }) =>
-          console.log('ScrollView height', nativeEvent.layout.height)
-        }>
-        <ProductOverview product={product} />
-        <BuyButton hasInventory={hasInventory} />
-        <ProductDescription
-          product={product}
-          handleDescriptionLayout={handleDescriptionLayout}
-        />
+          setScrollViewHeight(nativeEvent.layout.height)
+        }
+        contentContainerStyle={contentContainerStyle}>
+        <View
+          onLayout={({ nativeEvent }) =>
+            setcontentScrollViewHeight(nativeEvent.layout.height)
+          }>
+          <ProductOverview product={product} />
+          <BuyButton hasInventory={hasInventory} />
+          <ProductDescription
+            product={product}
+            handleDescriptionLayout={handleDescriptionLayout}
+          />
+        </View>
       </StyledScrollView>
-      <AccordionButton
-        onOpenAccordion={handleAccordionPress}
-        isAccordionOpen={isAccordionOpen}
-      />
+      {hasAccordion && (
+        <AccordionButton
+          isAccordionOpen={isAccordionOpen}
+          onOpenAccordion={handleAccordionPress}
+        />
+      )}
     </>
   );
 };
