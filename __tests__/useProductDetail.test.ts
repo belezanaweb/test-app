647f9b737664dd3d552624b77fe13f27a03337f7
octavio tosta <--global>
2021-02-10 08:33:53 -0300

    bugfix: fix file names

--- __tests__/useProductDetail.test.ts
+++ __tests__/useProductDetail.test.ts
@@ -1,65 +1,24 @@
 import { act, cleanup, renderHook } from '@testing-library/react-hooks';
-import { useProductsList } from 'utils/customHooks/useProducts';
+import { useProductDetail } from 'utils/customHooks/useProducts';
 
-// If need render providers
-// import 'jest-styled-components';
-// import 'react-native-gesture-handler/jestSetup';
-// const MainStack = createStackNavigator<MainStackParamList>();
-// jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
-// jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
-// jest.mock('react-native-reanimated', () => {
-//   const Reanimated = require('react-native-reanimated/mock');
-//   Reanimated.default.call = () => {};
-//   return Reanimated;
-// });
-
-describe('Test of useProducts custom hook', () => {
+describe('Test of useProductDetail custom hook', () => {
   test('Should render hook without re render and loadings on', async () => {
-    const { result } = renderHook(() => useProductsList(1, 10));
-    expect(result.current[0]).toHaveLength(0);
-    expect(result.current[1]).toBe(1);
-    expect(result.current[2]).toBe(true);
-    expect(result.current[3]).toBe(true);
-    expect(typeof result.current[4]).toBe('function');
+    const { result } = renderHook(() => useProductDetail());
+    expect(result.current[0]).toBeUndefined();
+    expect(result.current[1]).toBe(true);
+    expect(typeof result.current[2]).toBe('function');
   }, 30000);
 
-  test('Should fetch first time without page and turn off loadings after', async () => {
-    const { result } = renderHook(() => useProductsList(1, 10));
+  test('Should fetch productBySku, return product and set loading to false', async () => {
+    const { result } = renderHook(() => useProductDetail());
 
     await act(async () => {
-      await result.current[4]();
+      await result.current[2]('54417');
     });
 
     act(() => {
-      expect(result.current[0]).toHaveLength(10);
-      expect(result.current[1]).toBe(1);
-      expect(result.current[2]).toBe(false);
-      expect(result.current[3]).toBe(false);
-    });
-    cleanup();
-  }, 30000);
-
-  test('Should fetch two times: first without page param and second with page that was returned by hook', async () => {
-    const { result } = renderHook(() => useProductsList(1, 10));
-
-    await act(async () => {
-      await result.current[4]();
-    });
-
-    act(() => {
-      expect(result.current[2]).toBe(false);
-      expect(result.current[2]).toBe(false);
-    });
-
-    await act(async () => {
-      await result.current[4](result.current[1] + 1);
-    });
-
-    act(() => {
-      expect(result.current[0]).toHaveLength(20);
-      expect(result.current[1]).toBe(2);
-      expect(result.current[2]).toBe(false);
-      expect(result.current[3]).toBe(false);
+      expect(result.current[0]).not.toBeUndefined();
+      expect(result.current[1]).toBe(false);
     });
     cleanup();
   }, 30000);
